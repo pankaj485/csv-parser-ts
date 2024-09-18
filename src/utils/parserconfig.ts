4e0cb5af73681a6444686de225a488fd62370d68
Pankaj Thapaliya <pankaj.thapaliya@enginecal.com>
2024-09-18 19:12:00 +0530

    enhancement: implement separeate variable for file path

--- src/utils/parserconfig.ts
+++ src/utils/parserconfig.ts
@@ -1,7 +1,9 @@
 import path from "path";
 import { ParserConfig } from "../types/parser";
 
+const uploadPath = "../../public/graph-mapping.csv";
+
 export const parserConfig: ParserConfig = {
   headerRow: 1,
-  filePath: path.resolve(__dirname, "../../public/graph-mapping.csv"),
+  filePath: path.resolve(__dirname, uploadPath),
 };
