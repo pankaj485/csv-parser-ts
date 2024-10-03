70e33559c38518766f2612e240565f82fb324079
pankaj485 <pankajsamar2@gmail.com>
2024-10-04 00:14:23 +0530

    implement API to get file header

--- src/routes/v1/app.route.ts
+++ src/routes/v1/app.route.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import { sayHello } from "../../controllers/app.controller";
-import { parseCsv } from "../../controllers/parser.controller";
+import { getCsvHeaders, parseCsv } from "../../controllers/parser.controller";
 
 const router: Router = express.Router();
 
 router.get("/", sayHello);
 router.get("/csv-parser", parseCsv);
+router.post("/get-csv-headers", getCsvHeaders);
 
 export { router as apiV1AppRoute };
