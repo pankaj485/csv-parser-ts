faf249d40e9a384f7ea38c1873611b054d1c1f6a
pankaj485 <pankajsamar2@gmail.com>
2024-10-04 00:13:47 +0530

    setup environment config

---
+++ src/environtment.d.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  APPWRITE_API_KEY: z.string(),
+  APPWRITE_BUCKET_ID: z.string(),
+  APPWRITE_PROJECT_ID: z.string(),
+});
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envSchema> {}
+  }
+}
